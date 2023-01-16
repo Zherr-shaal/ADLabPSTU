6ea81cd5eec368a0dc1469fb4f1f349aeb201f9f
Zherr-shaal <55242358+Zherr-shaal@users.noreply.github.com>
2023-01-16 14:04:31 +0500

    Создание канала для вывода уведомления

--- app/src/main/java/com/asuris19/recorder/activities/MainActivity.java
+++ app/src/main/java/com/asuris19/recorder/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.asuris19.recorder.activities;
 
 import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -59,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        createNotificationChannel();
         changeFragment(new RecordListFragment());
     }
 
@@ -85,5 +88,15 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return true;
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("Recording", "Recording", importance);
+            channel.setDescription("Уведомление о записи");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
